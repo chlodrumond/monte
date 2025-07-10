61d4ab355dfa2b091239759a18645c4f44c9b8c7
chlodrumond1 <44329385-chlodrumond1@users.noreply.replit.com>
2025-07-10 16:33:34 +0000

    Restored to '1649c20877e70a139055ec82a2fb954e279cca9b'
    Replit-Restored-To: 1649c20877e70a139055ec82a2fb954e279cca9b

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monte_platform.settings')
-
-application = get_wsgi_application()
-app = application  # For gunicorn compatibility
